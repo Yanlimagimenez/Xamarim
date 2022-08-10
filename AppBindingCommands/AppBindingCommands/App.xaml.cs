a7518b447820d78403ad51c29b58ed66008507c9
Yanlimagimenez <yan.limma2004@gmail.com>
2022-08-09 22:45:59 -0300

    Xamarim

---
+++ AppBindingCommands/AppBindingCommands/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBindingCommands
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+
+            DateTime data = DateTime.Now;
+            Application.Current.Properties["dtAtual"] = data;
+            Application.Current.Properties["AcaoInicial"] = string.Format(" +  App executado às {0}. \n",data );
+
+            MainPage page = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            Application.Current.Properties["AcaoStart"] =
+                string.Format(" * App iniciado às {0}. \n", DateTime.Now);
+        }
+
+        protected override void OnSleep()
+        {
+            Application.Current.Properties["AcaoSleep"] =
+                string.Format(" * App em segundo plano às {0}. \n", DateTime.Now);
+        }
+
+        protected override void OnResume()
+        {
+            Application.Current.Properties["AcaoResume"] =
+                string.Format(" * App reativado às {0}. \n", DateTime.Now);
+        }
+    }
+}
---
+++ AppBindingCommands/AppBindingCommands/MainPageViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using Xamarin.Forms;
+using System.Windows.Input;
+
+namespace AppBindingCommands
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public ICommand ShowMessageCommand;
+
+        private string name = string.Empty;
+        public string Name
+        { 
+            get { return name; } //get => name
+            set
+            {
+                if (name == null)
+                    return;
+
+                name = value;
+                OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(DisplayName));
+            }
+        }
+
+        private string displayMessage = string.Empty;
+
+        public string DisplayMessage
+        {
+            get => displayMessage;
+            set
+            {
+                if (displayMessage == null)
+                    return;
+
+                displayMessage = value;
+                OnPropertyChanged(nameof(DisplayMessage));
+            }
+        }
+
+        public void ShowMessage()
+        {
+            string dataProperty = Application.Current.Properties["dtAtual"].ToString();
+            DisplayMessage = $"Boa noite {Name}. Hoje é {dataProperty}.";
+        }
+
+        public MainPageViewModel()
+        {
+            ShowMessageCommand = new Command(ShowMessage);
+        }
+
+        public string DisplayName => $"Nome digitado : {Name}";
+    }
+
+    
+}
